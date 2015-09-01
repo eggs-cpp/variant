af95784b6485e4739cef9bdbba3ade216645f464
K-ballo <k@fusionfenix.com>
2015-09-01 11:28:59 -0300

    Disable use of std::is_trivially_copyable for libstdc++

--- include/eggs/variant/detail/config/prefix.hpp
+++ include/eggs/variant/detail/config/prefix.hpp
@@ -234,7 +234,7 @@
 
 /// std::is_trivially_copyable support
 #ifndef EGGS_CXX11_STD_HAS_IS_TRIVIALLY_COPYABLE
-#  if defined(__GLIBCXX__) && __GLIBCXX__ < 20150422
+#  if defined(__GLIBCXX__)
 #    define EGGS_CXX11_STD_HAS_IS_TRIVIALLY_COPYABLE 0
 #  elif defined(_CPPLIB_VER) && _CPPLIB_VER < 650
 #    define EGGS_CXX11_STD_HAS_IS_TRIVIALLY_COPYABLE 0
